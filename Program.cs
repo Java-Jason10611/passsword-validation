c68005181d732908b8a8cf268a8740bcbedfa9dd
r0flcopt0rz <r0flcopt0rz10611@gmail.com>
2020-09-25 11:53:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace passsword_validation
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        }
+
+        private static bool ValidatePassword(string password, out string Message)
+        {
+            var input = password;
+            Message = string.Empty;
+            Console.Write("please enter a password");
+            input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Password should not be empty");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{7,12}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!]");
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                Message = "error";
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                Message = "error";
+                return false;
+            }
+            else if (!hasMiniMaxChars.IsMatch(input))
+            {
+                Message = "error";
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                Message = "error";
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                Message = "error";
+                return false;
+            }
+            else
+            {
+                Message = "Password valid and accepted";
+                return true;
+
+            }
+        }
+    }
+}
